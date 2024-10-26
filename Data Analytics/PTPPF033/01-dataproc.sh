f03f8fd28dfee313685a74c211e6c6f744b01cb8
Jake Holmquist <jake.holmquist@gmail.com>
2024-10-26 12:27:29 -0400

    x

---
+++ Data Analytics/PTPPF033/01-dataproc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Task 1. Run a Dataproc Serverless Spark batch template workload
+
+# Submit a Spark batch workload using the Dataproc Cloud Storage to 
+# BigQuery template to import the IUCR reference data into a BigQuery 
+# table named chicago_iucr_ref under the crimes_ds dataset in your lab project.
+
+gcloud 
